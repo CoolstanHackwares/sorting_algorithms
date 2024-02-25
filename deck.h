56b424b10706c860e1a21999defc89c21f10957e
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-02-25 01:17:58 +0100

    Dealer

--- deck.h
+++ deck.h
@@ -1,6 +1,10 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef enum kind_e
 {
 	SPADE = 0,
@@ -22,10 +26,24 @@ typedef struct deck_node_s
 	struct deck_node_s *next;
 } deck_node_t;
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ **/
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void sort_deck(deck_node_t **deck);
 size_t list_len_deck(deck_node_t *list);
 int _strcmp(const char *s1, const char *s2);
 int card_value(deck_node_t *node);
 
 #endif /* DECK_H */
-
